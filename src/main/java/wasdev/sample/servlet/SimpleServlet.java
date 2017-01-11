5058fcf803ba3b6b3d07c827849994a9ebe1788e
akollapudi <akollapudi@miraclesoft.com>
2017-01-11 07:58:11 -0500

    Update SimpleServlet.java

--- src/main/java/wasdev/sample/servlet/SimpleServlet.java
+++ src/main/java/wasdev/sample/servlet/SimpleServlet.java
@@ -7,9 +7,12 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import wasdev.sample.functions.*;
 
 /**
- * Servlet implementation class SimpleServlet
+ * Servlet implementation class SimpleServlet..
+  Servlet implementation class SimpleServlet
+  New comment has been added.
  */
 @WebServlet("/SimpleServlet")
 public class SimpleServlet extends HttpServlet {
@@ -20,8 +23,9 @@ public class SimpleServlet extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Message message = new Message();
         response.setContentType("text/html");
-        response.getWriter().print("Hello World Watson!");
+        response.getWriter().print("Hello World "+message.getMessage()+"!");
     }
 
 }
